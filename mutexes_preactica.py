bcaaf9f039d1c76640fcbd6b762fde6a1abff430
Mauricio1932 <201227@ids.upchiapas.edu.mx>
2022-09-22 00:00:21 -0500

    second commit

--- mutexes_preactica.py
+++ mutexes_preactica.py
@@ -1,32 +1,32 @@
-from threading import Thread, Semaphore
+import threading
 from pytube import YouTube
-semaforo = Semaphore(1)
 
+mutex = threading.Lock()
 
-
-def descarga(video):
-    yt = YouTube(video)
+def descarga(vid):
+    yt = YouTube(vid)
     video=  yt.streams.filter(file_extension='mp4').order_by('resolution').first()
     video.download(destino)
     print("video descargado en: "+destino)
 
-class Hilo(Thread):
+class Hilo(threading.Thread):
     def __init__(self, id):
-        Thread.__init__(self)
+        threading.Thread.__init__(self)
         video=id
         self.id=video
 
-    def run(self):
-        semaforo.acquire()
-        descarga(self.id)
-        semaforo.release()
 
+    def run(self):
+        mutex.acquire()
+        descarga(self.id)
+        mutex.release()
 
 destino=r"C:/Users/alexm/Downloads/descargas_python"
 
-threads_semaphore = [Hilo('https://youtu.be/B02yu8ih-C0'), Hilo('https://youtu.be/enup62u1LEk'), Hilo('https://youtu.be/IQPhLCkUugw'), Hilo('https://youtu.be/lFw6sxMGIHk'),
+Hilos = [Hilo('https://youtu.be/B02yu8ih-C0'), Hilo('https://youtu.be/enup62u1LEk'), Hilo('https://youtu.be/IQPhLCkUugw'), Hilo('https://youtu.be/lFw6sxMGIHk'),
 Hilo('https://youtu.be/p5709BE-fR8'), Hilo('https://youtu.be/YMkGOwosPgw'), Hilo('https://youtu.be/geg3CsgXC8Q'), Hilo('https://youtu.be/sZpJ_4lj0y8'), Hilo('https://youtu.be/yoWISASmVHw'),
 Hilo('https://youtu.be/ETa2xOLFzeM')]
 
-for t in threads_semaphore:
-    t.start()
+
+for h in Hilos:
+    h.start()
